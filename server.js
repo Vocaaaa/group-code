43710fcfbdcf523f7353bfccbd2ac125abc0c753
Oliver <reeidcsgo@gmail.com>
2022-01-25 11:57:10 +0200

    aaaa

--- server.js
+++ server.js
@@ -27,48 +27,17 @@ server.get("/register", (req, res)=> {
         password: password
     });
 })
+server.get("/login", (req, res) => {
+    
+})
 server.get("/login", (req, res)=> {
     res.redirect("/public" + "/home.html")
 })
-
 server.get("/login", (req, res) => {
     res.sendFile(__dirname + "/public" + "/login.html")
 })
-server.get("/update", (req, res) => {
-    res.sendFile(__dirname + "/public" + "/updateuser.html")
-})
 server.get("/home", (req, res) => {
     res.sendFile(__dirname + "/public" + "/home.html")
-})
-server.get("/deleteuser", (req, res) => {
-    res.sendFile(__dirname + "/public" + "/deleteuser.html")
-})
-
-server.post("/delete", (req, res)=>{
-    const deleteEmail = req.body.delete-email
-    const deletePassword = req.body.delete-password
-    async function deleteUser() {
-        const deletedUser = await Users.findOneAndDelete({email:deleteEmail, password:deletePassword})
-        if(deleteUser){
-            res.send("User deleted")
-        } else {
-            res.send("Oops, something went wrong")
-        }
-    }
-    deleteUser();
-})
-server.post("/update", (req, res) => {
-    const oldName = req.body.old_name
-    const newName = req.body.new_name
-    async function updateUser() {
-        const updatedName = await Users.findOneAndUpdate({name:oldName}, {name:newName})
-        if(updatedName) {
-            res.send("Username updated")
-        } else {
-            res.send("Oops, something went wrong")
-        }
-    }
-    updateUser();
 })
 
 server.listen(3000)
